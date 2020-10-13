68f7cbb4a8e1300fa330dbe69865934e0253277c
Arturo Sotelo Bayangos <ambibayangos@gmail.com>
2020-10-13 19:29:08 +1300

    initial commit

---
+++ Magnetic_Door_Opener/Magnetic_Door_Opener/main.c
@@ -0,0 +1,18 @@
+/*
+ * Magnetic_Door_Opener.c
+ *
+ * Created: 13/10/2020 7:28:26 PM
+ * Author : ambib
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
